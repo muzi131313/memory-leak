51b554cb51876ecaf4ca4749157475c2c9ccdf4a
roastwind <muzi131313@163.com>
2020-04-27 20:52:02 +0800

    feat: finish vue spa memory leak example

--- vue-spa/src/router/index.ts
+++ vue-spa/src/router/index.ts
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import Home from '../views/Home.vue'
 
+import ItemContainer from '../views/containers/item-container.vue'
+import ProjectContainer from '../views/containers/project-container.vue'
+
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
@@ -13,10 +16,29 @@ const routes: Array<RouteConfig> = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: ItemContainer,
+    children: [
+      {
+        path: 'project',
+        name: 'project',
+        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+      },
+      {
+        path: 'projects',
+        name: 'projects',
+        component: ProjectContainer,
+        children: [
+          {
+            path: 'images',
+            name: 'images',
+            // route level code-splitting
+            // this generates a separate chunk (about.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+          }
+        ]
+      }
+    ]
   }
 ]
 
--- vue-spa/src/views/About.vue
+++ vue-spa/src/views/About.vue
@@ -1,5 +1,52 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button id="btn" @click="back">back</button>
+    <ul id="ul" ref="ul">
+      <li v-for="(image, key) in images" :key="key">
+        image: {{image.src}}
+        <image :src="image.src" width="100px"></image>
+      </li>
+    </ul>
   </div>
 </template>
+<script>
+const NUM = 10000
+
+export default {
+  name: 'About',
+  data () {
+    return {
+      images: []
+    }
+  },
+  created () {
+    this.initData()
+  },
+  methods: {
+    initData () {
+      for (let i = 0; i < NUM; i++) {
+        this.images.push({
+          src: 'https://alipic.lanhuapp.com/webd6b1e153-7375-4843-9c46-c520a1c36127?x-oss-process=image/resize,h_234/format,webp'
+        })
+      }
+    },
+    back () {
+      this.$router.back()
+    }
+  }
+}
+</script>
+<style scoped>
+#btn {
+  margin: 30px 0;
+}
+#ul {
+  height: 100px;
+  overflow-y: scroll;
+}
+#ul li {
+  width: 100%;
+  height: 30px;
+}
+</style>
